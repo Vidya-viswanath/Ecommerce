9344c73bf6c644b87efa1193e641f6a8f756f2f9
Vidya-viswanath <vidyaviswanath111@gmail.com>
2022-09-18 21:45:37 -0700

    herku added

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n94x@py#llt4@*^62pa%k#z7&s_mjl=(uxik@e0lys-371@#se
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ecommerce.urls'
@@ -118,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
